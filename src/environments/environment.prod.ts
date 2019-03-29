b2ae82b8e4278a041a46161c0677b29e8077a4d9
tranquoc113 <tranquoc631@gmail.com>
2019-03-29 21:12:00 +0700

    change

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyAi0omDMyfafoIAb4t4lsDHhl2mn4QsLwg",
+    authDomain: "devupdate-20462.firebaseapp.com",
+    databaseURL: "https://devupdate-20462.firebaseio.com",
+    projectId: "devupdate-20462",
+    storageBucket: "devupdate-20462.appspot.com",
+    messagingSenderId: "499427564292"
+  }
 };
